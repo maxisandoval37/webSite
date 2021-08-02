09a9dfbdef953e9d14bf3488246e75b999fa7659
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-08-01 21:58:38 -0300

    added easter eggs

---
+++ easterEgg/function.js
@@ -0,0 +1,40 @@
+let date = new Date();
+
+const iframe = document.createElement("iframe");            
+iframe.width = "100%";
+iframe.height = "890px";
+
+switch (date.getMonth()+1) {
+
+    case 1:
+        if (date.getDay()+1 >= 1 && date.getDay()+1 <= 7){
+            iframe.src = "easterEgg/newyear/newyear.html";
+            document.getElementById('easterEgg').appendChild(iframe);
+        }
+        break;  
+
+    case 2:
+        if (date.getDay()+1 >= 1 && date.getDay()+1 <= 16){
+            iframe.src = "easterEgg/valentine/valentine.html";
+            document.getElementById('easterEgg').appendChild(iframe);
+        }
+        break;
+
+    case 4:
+        if (date.getDay()+1 >= 1 && date.getDay()+1 <= 22){
+            iframe.src = "easterEgg/easter/easter.html";
+            document.getElementById('easterEgg').appendChild(iframe);
+        }
+        break;
+
+    case 12:
+        if (date.getDay()+1 >= 15 && date.getDay()+1 <= 27){
+            iframe.src = "easterEgg/christmas/christmas.html";
+            document.getElementById('easterEgg').appendChild(iframe);
+        }
+        break;
+
+    default:
+        break;
+        
+}
