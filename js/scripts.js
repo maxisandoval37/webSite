511ccde778b585f455696cee4e707cdd8e57a155
Maximiliano Sandoval <maxisandoval98@gmail.com>
2022-04-23 14:23:00 -0300

    remove old elements and optimization

--- js/scripts.js
+++ js/scripts.js
@@ -80,59 +80,7 @@ $(document).on('click', '.filter-button', function () {
 
     }
 });
-$(".popup-img").popupimg({
-    openEffect: "none",
-    closeEffect: "none"
-});
 
-$(document).ready(function () {
-    $(document).on('click', '#submit_contact', function () {
-        var name = $('#name').val();
-        var email = $("#email").val();
-        var subject = $("#subject").val();
-        var tel = $("#tel").val();
-        var message = $("#message").val();
-
-        if (name != '' && email != '' && tel != '' && message != ''){
-            $.ajax({
-                url: "send_mail_contact.php",
-                method: "POST",
-                data: $('#contact-form').serialize(),
-                success: function (data) {
-                    $(".successmsg").show().delay(5000).fadeOut();
-                    $("#contact-form")[0].reset();
-                }
-            });
-        }
-    });
-});
-function KeycheckOnlyNumeric(e){
-    var _dom = 0;
-    _dom = document.all ? 3 : (document.getElementById ? 1 : (document.layers ? 2 : 0));
-    if (document.all)
-        e = window.event;
-    var ch = '';
-    var KeyID = '';
-    if (_dom == 2) {
-        if (e.which > 0)
-            ch = '(' + String.fromCharCode(e.which) + ')';
-        KeyID = e.which;
-    }
-    else{
-        if (_dom == 3) {
-            KeyID = (window.event) ? event.keyCode : e.which;
-        }
-        else {
-            if (e.charCode > 0)
-                ch = '(' + String.fromCharCode(e.charCode) + ')';
-            KeyID = e.charCode;
-        }
-    }
-    if ((KeyID >= 65 && KeyID <= 90) || (KeyID >= 97 && KeyID <= 122) || (KeyID >= 33 && KeyID <= 47) || (KeyID >= 58 && KeyID <= 64) || (KeyID >= 91 && KeyID <= 96) || (KeyID >= 123 && KeyID <= 126) || (KeyID == 32)){
-        return false;
-    }
-    return true;
-}
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {
         $('#scroll-to-top').fadeIn(200);
