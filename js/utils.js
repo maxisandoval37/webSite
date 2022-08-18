cf614f75c48d0039c17660d53d8b1d58b074bc6e
Maxi Sandoval <maxisandoval98@gmail.com>
2022-08-17 22:10:58 -0300

    fixed redirect to https and check is current url is localhost

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,11 @@
 function redirectToHTTPS(){
     var urlHTTP = "http://";
     var urlCurrent = location.href;
-    if (urlCurrent.includes(urlHTTP)) {
+    if (urlCurrent.includes(urlHTTP) && !(isLocalHost(urlCurrent))) {
         location.href = urlCurrent.replace("http", "https");
     }
+}
+
+function isLocalHost(str){
+    return ((/\d/.test(str)) || (str.includes("local")))
 }
